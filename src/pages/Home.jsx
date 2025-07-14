a4c9209fe8150b8c7cd9242ee9d3a1c1bdc536d7
Monil501 <monilmodi501@gmail.com>
2025-07-14 11:53:19 +0530

    final

---
+++ src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ThemeToggle from '../components/ThemeToggle'
+import StarBackground from '../components/StarBackground'
+import Navbar from '../components/Navbar'
+import HeroSection from '../components/HeroSection'
+import AboutSection from '../components/AboutSection'
+import SkillsSection from '../components/SkillsSection'
+import ProjectSection from '../components/ProjectSection'
+import ContactSection from '../components/ContactSection'
+
+const Home = () => {
+  return (
+    <div className='min-h-screen bg-backgroung text-foreground overflow-x-hidden'>
+      <ThemeToggle />
+      <StarBackground />
+      <Navbar />
+      <main>
+          <HeroSection />
+          <AboutSection />
+          <SkillsSection />
+          <ProjectSection />
+          <ContactSection />
+      </main>
+    </div>
+  )
+}
+
+export default Home
